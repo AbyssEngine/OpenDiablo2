9838175a6f2e3424e10a46dd84c59e6dbbccf5be
Alexey Sokolov <sokolov@google.com>
2022-01-02 00:24:36 +0000

    Support Russian version

--- common/language.lua
+++ common/language.lua
@@ -4,15 +4,21 @@ local Language = {}
 local _languageDefs = require('common/enum/language')
 local _code, _3code, _name, _strings
 
-local function loadD2RStrings()
-    --TODO load other json files from this directory
-    if not abyss.fileExists('/data/local/lng/strings/ui.json') then
+local function loadJsonTranslations(filename)
+    local path = '/data/local/lng/strings/' .. filename
+    if not abyss.fileExists(path) then
         return
     end
-    local json = ReadJsonAsTable('/data/local/lng/strings/ui.json')
+    local json = ReadJsonAsTable(path)
     for _, data in ipairs(json) do
         _strings[data.Key] = data[_code]
     end
+end
+
+local function loadD2RStrings()
+    --TODO load other json files from this directory
+    loadJsonTranslations('ui.json')
+    loadJsonTranslations('levels.json')
 end
 
 local function loadTblFile(path)
@@ -47,6 +53,8 @@ function Language:setLanguage(code)
 
     loadTblStrings()
     loadD2RStrings()
+    -- just for case, because :translate() relies on this empty key not to exist for @#num to work
+    _strings[''] = nil
 end
 
 --- Auto detect the langauge based on files in the path
@@ -72,38 +80,44 @@ function Language:code()
     return _code
 end
 
--- Takes string such as "@foo","@bar#nnn" or "@#nnn" and replaces it with their translation
+-- Takes string which contains things such as "@foo","@bar#nnn" or "@#nnn" and
+-- replaces it with their translation.
 -- nnn is the number; mostly it's for compatibility with MPQs, as not all keys there are strings.
 -- @text#nnn form uses text if available, and falls back to nnn
 -- numeric code only uses tbl files, text code tries json file first, then tbl,
 -- because the numeric code in json doesn't always match the numeric code in
 -- tbl, and some strings are only accessible in tbl via number
 --
--- TODO consider automaically mapping the code to json too.
+-- TODO consider automatically mapping the code to json too.
 -- So far, seems like numbers in the 5xxx range are the same, but numbers in
--- 2xxx range are prepended with another 2, turning e.g. 2519 into 22519 in
--- json
+-- 2xxx range (from LOD expansion) are prepended with another 2, turning e.g.
+-- 2519 into 22519 in json
+local _translatePattern
 function Language:translate(str)
-    local m, _, code, num = str:find('^@(%w+)(#%d+)$')
-    if m == nil then
-        m, _, num = str:find('^@(#%d+)$')
-    end
-    if m == nil then
-        m, _, code = str:find('^@(%w+)$')
-    end
-    if m == nil then
-        return str
-    end
-    local result = _strings[code]
-    if result ~= nil then
-        return result
-    end
-    result = _strings[num]
-    if result ~= nil then
-        return result
-    end
-    return str
+    return _translatePattern:match(str)
 end
+local function _createTranslatePattern()
+    local P = lpeg.P
+    local C = lpeg.C
+    local Cs = lpeg.Cs
+    local R = lpeg.R
+
+    local function replace(code, num)
+        local result = _strings[code]
+        if result ~= nil then
+            return result
+        end
+        result = _strings[num]
+        if result ~= nil then
+            return result
+        end
+        return '??@' .. code .. '#' .. num .. '??'
+    end
+    local letter = R'az' + R'AZ' + R'09'
+    local token = P'@' * C(letter^0) * C(P'#' * R'09'^0)^-1
+    return Cs((token / replace + 1)^0)
+end
+_translatePattern = _createTranslatePattern()
 
 function Language:hdaudioPath(originalPath)
     if _code == "enUS" then
